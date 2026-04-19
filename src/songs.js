e3055648b1a821c4ac201cfffd5c26368b2a4052
Sarthak <Sarthakbhopale1703@gmail.com>
2026-04-19 19:57:55 +0530

    feat: add initial predefined songs library

---
+++ src/songs.js
@@ -0,0 +1,75 @@
+// Predefined songs in JSON format
+export const songs = [
+  {
+    id: 'demo-1',
+    title: 'Ode to Joy',
+    notes: [
+      { note: 'E4', time: 0, duration: 400 },
+      { note: 'E4', time: 500, duration: 400 },
+      { note: 'F4', time: 1000, duration: 400 },
+      { note: 'G4', time: 1500, duration: 400 },
+      { note: 'G4', time: 2000, duration: 400 },
+      { note: 'F4', time: 2500, duration: 400 },
+      { note: 'E4', time: 3000, duration: 400 },
+      { note: 'D4', time: 3500, duration: 400 },
+      { note: 'C4', time: 4000, duration: 400 },
+      { note: 'C4', time: 4500, duration: 400 },
+      { note: 'D4', time: 5000, duration: 400 },
+      { note: 'E4', time: 5500, duration: 400 },
+      { note: 'E4', time: 6000, duration: 600 },
+      { note: 'D4', time: 6750, duration: 200 },
+      { note: 'D4', time: 7000, duration: 800 }
+    ]
+  },
+  {
+    id: 'demo-2',
+    title: 'Twinkle Twinkle',
+    notes: [
+      { note: 'C4', time: 0, duration: 400 },
+      { note: 'C4', time: 500, duration: 400 },
+      { note: 'G4', time: 1000, duration: 400 },
+      { note: 'G4', time: 1500, duration: 400 },
+      { note: 'A4', time: 2000, duration: 400 },
+      { note: 'A4', time: 2500, duration: 400 },
+      { note: 'G4', time: 3000, duration: 800 },
+      { note: 'F4', time: 4000, duration: 400 },
+      { note: 'F4', time: 4500, duration: 400 },
+      { note: 'E4', time: 5000, duration: 400 },
+      { note: 'E4', time: 5500, duration: 400 },
+      { note: 'D4', time: 6000, duration: 400 },
+      { note: 'D4', time: 6500, duration: 400 },
+      { note: 'C4', time: 7000, duration: 800 }
+    ]
+  },
+  {
+    id: 'demo-3',
+    title: 'Happy Birthday',
+    notes: [
+      { note: 'C4', time: 0, duration: 300 },
+      { note: 'C4', time: 400, duration: 100 },
+      { note: 'D4', time: 500, duration: 400 },
+      { note: 'C4', time: 1000, duration: 400 },
+      { note: 'F4', time: 1500, duration: 400 },
+      { note: 'E4', time: 2000, duration: 800 },
+      { note: 'C4', time: 3000, duration: 300 },
+      { note: 'C4', time: 3400, duration: 100 },
+      { note: 'D4', time: 3500, duration: 400 },
+      { note: 'C4', time: 4000, duration: 400 },
+      { note: 'G4', time: 4500, duration: 400 },
+      { note: 'F4', time: 5000, duration: 800 },
+      { note: 'C4', time: 6000, duration: 300 },
+      { note: 'C4', time: 6400, duration: 100 },
+      { note: 'C5', time: 6500, duration: 400 },
+      { note: 'A4', time: 7000, duration: 400 },
+      { note: 'F4', time: 7500, duration: 400 },
+      { note: 'E4', time: 8000, duration: 400 },
+      { note: 'D4', time: 8500, duration: 800 },
+      { note: 'A#4', time: 9500, duration: 300 },
+      { note: 'A#4', time: 9900, duration: 100 },
+      { note: 'A4', time: 10000, duration: 400 },
+      { note: 'F4', time: 10500, duration: 400 },
+      { note: 'G4', time: 11000, duration: 400 },
+      { note: 'F4', time: 11500, duration: 800 }
+    ]
+  }
+];
